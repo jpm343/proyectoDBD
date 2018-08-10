a65c7b65e2a6a16aef9533e8b11498be141d1176
Salazarkss <sebastian.salazar.v@usach.cl>
2018-08-10 19:10:43 -0400

    Probando

---
+++ database/migrations/2018_08_10_023215_create_vuelos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVuelosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vuelos', function (Blueprint $table) {
+            $table->increments('id_vuelo');
+            $table->datetime('fecha_salida');
+            $table->datetime('fecha_llegada');
+            $table->string('ciudad_origen');
+            $table->string('ciudad_destino');
+            $table->string('aeropuerto_origen');
+            $table->string('aeropuerto_destino');
+            $table->string('pais_origen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vuelos');
+    }
+}
