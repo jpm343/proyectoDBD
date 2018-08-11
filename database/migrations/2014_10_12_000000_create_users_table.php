f8bce4a8ddfedd52f72f16774741842bc8bbdb30
Alexander <25644992+apalmalarrain@users.noreply.github.com>
2018-08-10 23:21:04 -0400

    Revertir config/database.php y eliminar create_users_table
    Cambios en config/database.php del ultimo commit invalidaban la
    configuracion local del archivo .env
    
    El archivo database/migrations/2014_10_12_000000_create_users_table.php
    existe en cada proyecto de Laravel recien creado, pero no sera usado en
    este proyecto - este commit asegura que la migracion no tenga efecto.

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /*
+     * Hacer nada!
+     *
+     * Dejo el archivo para asegurar que nadie
+     *     realice la migracion a pesar de
+     *     existir por defecto
+     */
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}
