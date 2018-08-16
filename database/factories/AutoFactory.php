490864e23e2cb2bcd1e2fb57c46e70798f317e41
Alexander <25644992+apalmalarrain@users.noreply.github.com>
2018-08-16 00:08:22 -0300

    Implementar Factories para Auto y CompaniaAuto

---
+++ database/factories/AutoFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Auto::class, function (Faker $faker) {
+    return [
+        'patente_auto' => $faker->asciify('********'),
+        'modelo_auto' => $faker->word(),
+        'capacidad_auto' => $faker->numberBetween($min = 2, $max = 7),
+        'precio_dia_auto' => $faker->randomNumber(),
+        'descripcion_auto' => $faker->text(),
+        'transmision_auto' => $faker->randomElement($array = array('A', 'M')),
+    ];
+});
