a8c0e0afb4fdabe2df785a8f3eedc28601cb814b
Alvaro Montecinos Lorca <alvaro.montecinos@usach.cl>
2018-08-16 02:39:43 -0300

    Agregados seeds y controladores, faltan funciones create y edit

---
+++ database/seeds/RolsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Rol;
+
+class RolsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Rol::class,4)->create();
+    }
+}
