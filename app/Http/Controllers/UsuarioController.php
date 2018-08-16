2574a0d322d30269384248d5e2b803695b91f901
Alvaro Montecinos Lorca <alvaro.montecinos@usach.cl>
2018-08-16 02:36:09 -0300

    Actualizados controladores, faltan funciones create y edit

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Usuario;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuario = Usuario::all();
+        return $usuario;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario = new Usuario();
+        $usuario->id_rol =  $request->id_rol;
+        $usuario->nombre_usuario = $request->nombre_usuario;
+        $usuario->correo_usuario = $request->correo_usuario;
+        $usuario->password_usuario = $request->password_usuario;
+        $usuario->save();
+        return $usuario;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $usuario = Usuario::find($id);
+        return $usuario;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->id_rol =  $request->id_rol;
+        $usuario->nombre_usuario = $request->nombre_usuario;
+        $usuario->correo_usuario = $request->correo_usuario;
+        $usuario->password_usuario = $request->password_usuario;
+        $usuario->save();
+        return $usuario;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->delete();
+        return Usuario::all();
+    }
+}
