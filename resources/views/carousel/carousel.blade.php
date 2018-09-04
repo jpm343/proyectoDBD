f17d41040ad190e4bf9c8ccbab46e46019d56053
jpm343 <37777904+jpm343@users.noreply.github.com>
2018-09-04 02:15:21 -0300

    Add files via upload

---
+++ resources/views/carousel/carousel.blade.php
@@ -0,0 +1,30 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/prueba1.jpg" width="700" height="400">
+    </div>
+    <div class="carousel-item">
+      <img src="./images/prueba2.jpg" width="700" height="400">
+    </div>
+    <div class="carousel-item">
+      <img src="./images/prueba3.jpg" width="700" height="400">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,72 +2,43 @@
 <html>
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Prueba</title>
+    <link rel="stylesheet" href=./css/bootstrap.min.css>
+    <link rel="stylesheet" href=  ./css/personalizado.css>
+    <title>Launching.com</title>
 </head>
 <body>
-  Crear:
-  <input type="button" onclick=" location.href='/vuelos' " value="Vuelo" name="boton1" />
-  <input type="button" onclick=" location.href='/aerolineas' " value="Aerolinea" name="boton2" /> 
-  <input type="button" onclick=" location.href='/asientos' " value="Asiento" name="boton3" /> 
-  <br>
-  Editar:
-  <input type="button" onclick=" location.href='/vuelos' " value="Vuelo" name="boton1" />
-  <input type="button" onclick=" location.href='/aerolineas' " value="Aerolinea" name="boton2" /> 
-  <input type="button" onclick=" location.href='/asientos' " value="Asiento" name="boton3" />
-  <br><br> 
-  <form action="/action_page.php">
-    <div class = "content">
-      <div class = "links">
-        <a href="{{route('Usuarios.index')}}"> Lista de Usuarios</a>
-         <a href="{{route('Rols.index')}}"> Lista de Roles</a>
-         <a href="{{route('RegistroConsultas.index')}}"> Lista de Registro_Consulta</a>
-      </div> 
-     </div>
-   </form>
-  Crear/Editar:
-  <input type="button" onclick=" location.href='/usuarios' " value="Usuarios" name="btn1" />
-  <input type="button" onclick=" location.href='/rols' " value="Rols" name="btn2" /> 
-  <input type="button" onclick=" location.href='/registroConsultas' " value="Registro Consultas" name="btn3" /> 
-  <br><br>
-  <form action="/action_page.php">
-      <div class = "content">
-        <div class = "links">
-          <a href="{{route('actividad.index')}}"> Lista de actividades</a>
-          <a href="{{route('registro.index')}}"> Lista de fondos</a>
-          <a href="{{route('fondo.index')}}"> Lista de Registros</a>
-        </div> 
-      </div>
-    <form action="/action_page.php">
-      
-      <div class="form-group">
-        <label for="email">Email address:</label>
-        <input type="email text-center" class="form-control" id="email">
-      </div>
-      
-      <div class="form-group">
-        <label for="pwd">Password:</label>
-        <input type="password text-center" class="form-control" id="pwd">
-      </div>
-      
-      <div class="form-group form-check">
-        <label class="form-check-label text-center">
-          <input class="form-check-input" type="checkbox"> Remember me
-        </label>
-      </div>
-      
-      <div class = "content">
-        <div class = "links">
-          <a href="{{route('Habitacion.index')}}"> Habitacion index</a>
-          <a href="{{route('Hotel.index')}}"> Hotel index</a>
-          <a href="{{route('Traslado.index')}}"> Traslado index</a>
-        </div> 
-      </div>
 
-      <button type="submit" class="btn btn-primary text-center">Submit</button>
-    </form>
-  Crear/Editar:
-  <input type="button" onclick=" location.href='/actividads' " value="Actividades" name="bton1" />
-  <input type="button" onclick=" location.href='/registros' " value="Registros" name="bton2" /> 
-  <input type="button" onclick=" location.href='/fondos' " value="Fondos" name="bton3" /> 
+   @include('navbar.navbar')
+<div>
+	<div class="dos-columnas" align="left">
+   		@include('carousel.carousel')
+	</div>
+	<div class="dos-columnas" align="right">
+		<form>
+		  <div>
+		   	<label for="Ciudad">Ingrese Ciudad</label>
+		   	<input type="text" name="Ciudad">
+		  </div>
+		  <div>
+		  	<label for="name">Apellido</label>
+		   	<input type="text" name="apellido">
+		  </div>
+		  <div>
+		   	<label for="msg">Mensaje</label>
+		   	<textarea id="msg"></textarea>
+		  </div>
+		  <div>
+		  	<label for="submit"></label>
+		  	<input type="submit">
+		  </div>
+		</form>
+	</div>
+</div>
+
 </body>
+
+<script src="./js/jquery-slim.min.js"></script>
+<script src="./js/popper.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+
 </html>
