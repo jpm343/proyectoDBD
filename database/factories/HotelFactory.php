04d3cbff3219c7b86e399ad4e680e34fee5392cd
Salazarkss <sebastian.salazar.v@usach.cl>
2018-09-05 21:49:19 -0300

    Agregacion Vista Alojamientos

--- database/factories/HotelFactory.php
+++ database/factories/HotelFactory.php
@@ -8,6 +8,6 @@ $factory->define(App\Hotel::class, function (Faker $faker) {
     	'puntuacion_hotel' 	=> rand(1, 2), 
     	'descripcion_hotel' => $faker->text(150), 
     	'direccion_hotel' 	=> $faker->text(150),
-    	'ciudad_hotel' 		=> $faker->text(150),
+    	'ciudad_hotel' 		=> $faker->word(),
     ];
 });
