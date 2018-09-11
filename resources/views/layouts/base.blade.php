c00f58278f8b00c08780b5ad2e43ba9aeb1bb5f8
Alexander <25644992+apalmalarrain@users.noreply.github.com>
2018-09-11 02:48:07 -0300

    Introducir un estilo visual provisional
    Basado en un esquema de colores compartido en WhatsApp.
    
    Emplea filter:invert para evitar modificar bootstrap directamente.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,10 +5,40 @@
         <link rel="stylesheet" href="/css/personalizado.css">
         <link rel="stylesheet" href="/css/bootstrap.min.css">
         <title>@yield('titulo') | Launching.com</title>
+        <style>
+            .heading {
+                background-color: #a85f65; /* #57a09a */
+                padding: 15px 0;
+                box-shadow: 0 7px 0 0 #703f56; /* #8fc0a9; */
+                margin-bottom: 50px;
+                -webkit-filter: invert(100%);
+                filter: invert(100%);
+            }
+
+            body {
+                background-color: #faf3dd;
+            }
+
+            .container input[type=submit], .container button {
+                background-color: #8fc0a9;
+            }
+
+            .carousel img {
+                border-radius: .375rem;
+            }
+
+            .content {
+                padding-bottom: 25vh;
+            }
+        </style>
     </head>
     <body>
-        <div class="container">
-            @include('navbar.navbar')
+        <div class="heading">
+            <div class="container">
+                @include('navbar.navbar')
+            </div>
+        </div>
+        <div class="container content">
             <h1 class="titulo">@yield('titulo')</h1>
             @yield('contenido')
         </div>
