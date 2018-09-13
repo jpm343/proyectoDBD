a5744c1b2e4f7179ce601f56a55baf2994016a35
Unknown <gabriel.toro@usach.cl>
2018-09-12 23:36:30 -0300

    Agregada la condición para agregar al carrito

---
+++ resources/views/Traslado_view/Traslado_fragment/Traslado-abrirSesion.blade.php
@@ -0,0 +1,6 @@
+<p class = "alert alert-info">
+	Recuerde que para solicitar un traslado debe iniciar sesión.
+	<button type="button" class = "close" data-dismiss="alert">
+			&times;
+	</button>
+</p>
