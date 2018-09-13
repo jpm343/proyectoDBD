2a84723357ffe5fd43e5bf0b0a838df33a36ed18
Unknown <gabriel.toro@usach.cl>
2018-09-13 00:28:37 -0300

    actualizando la rama gaboBranch

--- resources/views/prueba_compra.blade.php
+++ resources/views/prueba_compra.blade.php
@@ -3,7 +3,7 @@
 		//array_push($_SESSION['carro'], $reserva);
 		$_SESSION['carro'][] = $reserva;
 		//var_dump($_SESSION['carro']);
-		header('Refresh: 1; URL=/');
+		header('Refresh: 1; URL=/carrito');
 	?>
 	Agregado al carro, redireccionando al inicio
 @endif
