2b8db42f03b1c62f6258194bdae78ffbc2c8a597
Unknown <gabriel.toro@usach.cl>
2018-09-13 00:06:42 -0300

    actualizando el navbar

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar nav-backgroud navbar-expand-lg navbar-light" id="navbar">
+<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
 <a class="navbar-brand" href="/">Launching.com</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
@@ -6,25 +6,26 @@
 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
   <div class="navbar-nav ml-auto">
     @guest
-    <img src=/images/usuario.png class="icono">
-    @if (Request::is('*login*'))
-    <a class="nav-item nav-link nav-font-style active" href="/login"><b>Iniciar Sesión</b></a>
-    @else
-    <a class="nav-item nav-link nav-font-style" href="/login">Iniciar Sesión</a>
-    @endif
-    @else
-    <a class="nav-item nav-link" href="/logout"
-        onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-            Cerrar sesión
-    </a>
-    <img src=/images/usuario.png class="icono">
-    @if (Request::is('*perfil*'))
-    <a class="nav-item nav-link active" href="perfil"><b>{{ Auth::user()->name }}</b></a>
-    @else
-    <a class="nav-item nav-link" href="perfil">{{ Auth::user()->name }}</a>
-    @endif
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+        <img src=/images/usuario.png class="icono">
+        @if (Request::is('*login*'))
+        <a class="nav-item nav-link active" href="/login"><b>Iniciar Sesión</b></a>
+        @else
+        <a class="nav-item nav-link" href="/login">Iniciar Sesión</a>
+        @endif
+        @else
+        <a class="nav-item nav-link" href="/logout"
+            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                Cerrar sesión
+        </a>
+        <img src=/images/usuario.png class="icono">
+        @if (Request::is('*perfil*'))
+        <a class="nav-item nav-link active" href="perfil"><b>{{ Auth::user()->name }}</b></a>
+        @else
+        <a class="nav-item nav-link" href="perfil">{{ Auth::user()->name }}</a>
+        @endif
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
     @endguest
+    
     <img src=/images/bolsa-de-la-compra.png class="icono">
     @if (Request::is('*carrito*'))
     <a class="nav-item nav-link active" href="/carrito"><b>Mis Compras</b></a>
@@ -34,7 +35,7 @@
   </div>
 </nav>
 
-<nav class="navbar nav-backgroud navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light">
 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
   <div class="navbar-nav mr-auto">
     @foreach ([
